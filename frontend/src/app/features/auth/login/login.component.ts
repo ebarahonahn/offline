b1a7d80b273a3a57a22bea09e42231e2ef571420
Edgar Ernesto Barahona Flores <edgar0371@gmail.com>
2026-04-06 06:41:13 -0600

    Cambios en el login

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -19,7 +19,7 @@ import { AuthService } from '../../../core/services/auth.service';
             </svg>
           </div>
           <h1 class="text-2xl font-bold text-gray-900 dark:text-white">Sistema Teletrabajo</h1>
-          <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">Control de tiempo laboral institucional</p>
+          <p class="mt-1 text-sm text-gray-500 dark:text-gray-400">Control de tiempo laboral institucionalll</p>
         </div>
 
         <!-- Card del formulario -->
